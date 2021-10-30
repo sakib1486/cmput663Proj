9a03a4d9164ab39bc9ef09ec42573bd9a0f3a455
sakib1486 <sakibiut134406@gmail.com>
2021-10-30 17:07:12 -0600

    Retrying

---
+++ jsontocsv.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import json
+import csv
+import os
+
+def main():
+
+	#path to the JSON file and project repositories
+	json_path = "Data/sstubsLarge"
+	proj_dir = "/media/sakib/478407FA0B573D27/JavaProj/"
+
+	filenames = os.listdir(proj_dir)
+	#print(filenames)
+	df = pd.read_json(json_path)
+
+	d = df[df.projectName.isin(filenames)]
+	d.to_csv(r"filteredTopProjectSStuBs.csv")
+	#print(d)
+	
+
+
+if __name__=='__main__':
+	main()
