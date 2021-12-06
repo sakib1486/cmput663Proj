c9e872ec0872efc72392496eac130f438b0b3376
Nazmus Sakeef <nazmussakeef1700@gmail.com>
2021-12-05 19:36:29 -0700

    Error added

---
+++ demoScriptsforAnalyze_error_added/Average.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Avg
+{
+	double res;
+	Avg(int  a[],int n)
+	{
+	     res=0;
+	     for(int i=0;i<n;i++)
+	        res =res+a[i];
+	    
+	}
+}
+class Average
+{
+	public static void main(String args[]) {
+		int n;double res=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter how many numbers to cal  avg");
+	   
+		n=sc.nextInt();
+		int a[]=new int[n];
+		System.out.println("enter   "+n+"  numbers");
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+
+		Avg k=new Avg(a,n);
+		%Bugs Added
+		Avg(a,n,n);
+		Avg(a)
+		System.out.println("Average ="+k.res);
+     }
+}
